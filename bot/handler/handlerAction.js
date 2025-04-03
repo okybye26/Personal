ae25045b4f3150e50ed123b6c223eb93a0ee1969
Ayan-alt-deep <sankarraj5900@gmail.com>
2025-04-03 06:01:14 +0600

    Add files via upload

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -1,62 +1,106 @@
-// Importing the necessary files
-const createFuncMessage = require("./path/to/your/createFuncMessage.js"); // <-- add the correct path
+const createFuncMessage = global.utils.message;
 const handlerCheckDB = require("./handlerCheckData.js");
-const request = require("request");
-const axios = require("axios");
-const fs = require("fs-extra");
+
+const request = require("request")
+const axios = require("axios")
+const fs = require("fs-extra")
+
 
 module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData) => {
-    const handlerEvents = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js")(api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData);
+	const handlerEvents = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js")(api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData);
+
+	return async function (event) {
+		const message = createFuncMessage(api, event);
+
+		await handlerCheckDB(usersData, threadsData, event);
+		const handlerChat = await handlerEvents(event, message);
+		if (!handlerChat)
+			return;
+
+		const { onStart, onChat, onReply, onEvent, handlerEvent, onReaction, typ, presence, read_receipt } = handlerChat;
+
+		switch (event.type) {
+			case "message":
+			case "message_reply":
+			case "message_unsend":
+				onChat();
+				onStart();
+				onReply();
+        if(event.type == "message_unsend"){
+          
+          let resend = await threadsData.get(event.threadID, "settings.reSend");
+		if (resend == true && event.senderID 
+!== api.getCurrentUserID()){
+      let umid = global.reSend[event.threadID].findIndex(e => e.messageID === event.messageID)
+      
+      if(umid>(-1)){
+let nname = await usersData.getName(event.senderID)
+        let attch = []
+if(global.reSend[event.threadID][umid].attachments.length>0){
+  let cn = 0
+  for(var abc of global.reSend[event.threadID][umid].attachments){
+   if(abc.type == "audio"){
     
-    const adminIDs = ["61574046213712"];  // Your UID
-    const prefix = "Eren";
+    cn += 1;
 
-    return async function (event) {
-        const message = createFuncMessage(api, event); // <-- Call to createFuncMessage here
-        await handlerCheckDB(usersData, threadsData, event);
-        const handlerChat = await handlerEvents(event, message);
-        if (!handlerChat) return;
+   let pts = `scripts/cmds/tmp/${cn}.mp3`
+					let res2 = (await axios.get(abc.url, {
+						responseType: "arraybuffer"
+					})).data;
+			fs.writeFileSync(pts, Buffer.from(res2, "utf-8"))
+    
+  attch.push(fs.createReadStream(pts))} else{
+     attch.push(await global.utils.getStreamFromURL(abc.url))
+  }
+  }
+}
+        
+  api.sendMessage({body: nname + " removed:\n\n" + global.reSend[event.threadID][umid].body,
+mentions:[{id:event.senderID, tag:nname}],
+    attachment:attch
+                  }, event.threadID)
+                   
 
-        const { onStart, onChat, onReply, onEvent, handlerEvent, onReaction, typ, presence, read_receipt } = handlerChat;
-
-        const isAdmin = adminIDs.includes(event.senderID);
-
-        if (!event.body) {
-            console.log("[DEBUG] No body in the event.");
-            return;
+  
+      }
+    }
         }
+				break;
+			case "event":
+				handlerEvent();
+				onEvent();
+				break;
+			case "message_reaction":
+				onReaction();
+        if(event.reaction == "❗"){
+  if(event.userID == "61561101500902"){
+api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
+                if (err) return console.log(err);
+              });
 
-        if (!event.body.startsWith(prefix) && !isAdmin) {
-            console.log("[DEBUG] Non-admin user trying to use command without prefix.");
-            return;
+}else{
+    message.send(":)")
+  }
+  }
+        if(event.reaction == "😠"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61574046213712"){
+    message.unsend(event.messageID)
+}else{
+    message.send(":)")
+  }}
         }
-
-        switch (event.type) {
-            case "message":
-            case "message_reply":
-            case "message_unsend":
-                onChat();
-                onStart();
-                onReply();
-                break;
-            case "event":
-                handlerEvent();
-                onEvent();
-                break;
-            case "message_reaction":
-                onReaction();
-                break;
-            case "typ":
-                typ();
-                break;
-            case "presence":
-                presence();
-                break;
-            case "read_receipt":
-                read_receipt();
-                break;
-            default:
-                break;
-        }
-    };
+				break;
+			case "typ":
+				typ();
+				break;
+			case "presence":
+				presence();
+				break;
+			case "read_receipt":
+				read_receipt();
+				break;
+			default:
+				break;
+		}
+	};
 };
