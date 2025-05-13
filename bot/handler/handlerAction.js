7976a73f10d65dcca7966f7ce6a43b6942c53ea0
Ayan-alt-deep <sankarraj5900@gmail.com>
2025-05-14 01:10:52 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -83,7 +83,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "😠"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61574046213712"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61575883992345){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
