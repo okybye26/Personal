811a447661f7321646cdccda6eb16a421eba3171
Ayan-alt-deep <sankarraj5900@gmail.com>
2025-05-19 21:57:07 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -1,13 +1,14 @@
 const createFuncMessage = global.utils.message;
 const handlerCheckDB = require("./handlerCheckData.js");
 
-const request = require("request")
-const axios = require("axios")
-const fs = require("fs-extra")
-
+const request = require("request");
+const axios = require("axios");
+const fs = require("fs-extra");
 
 module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData) => {
-	const handlerEvents = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js")(api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData);
+	const handlerEvents = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js")(
+		api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData
+	);
 
 	return async function (event) {
 		const message = createFuncMessage(api, event);
@@ -22,83 +23,98 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
 		switch (event.type) {
 			case "message":
 			case "message_reply":
-			case "message_unsend":
+			case "message_unsend": {
+				// Check noPrefixMode logic
+				const threadData = await threadsData.get(event.threadID);
+				const noPrefixMode = threadData?.data?.noPrefixMode === true;
+				const prefix = global.config.PREFIX || "!";
+				const body = event.body || "";
+
+				if (noPrefixMode && !body.startsWith(prefix)) {
+					const commandName = body.split(" ")[0].toLowerCase();
+					const command = global.client.commands.get(commandName) || global.client.aliases.get(commandName);
+					if (!command) return; // Ignore random text when noPrefix is on
+				}
+
 				onChat();
 				onStart();
 				onReply();
-        if(event.type == "message_unsend"){
-          
-          let resend = await threadsData.get(event.threadID, "settings.reSend");
-		if (resend == true && event.senderID 
-!== api.getCurrentUserID()){
-      let umid = global.reSend[event.threadID].findIndex(e => e.messageID === event.messageID)
-      
-      if(umid>(-1)){
-let nname = await usersData.getName(event.senderID)
-        let attch = []
-if(global.reSend[event.threadID][umid].attachments.length>0){
-  let cn = 0
-  for(var abc of global.reSend[event.threadID][umid].attachments){
-   if(abc.type == "audio"){
-    
-    cn += 1;
 
-   let pts = `scripts/cmds/tmp/${cn}.mp3`
-					let res2 = (await axios.get(abc.url, {
-						responseType: "arraybuffer"
-					})).data;
-			fs.writeFileSync(pts, Buffer.from(res2, "utf-8"))
-    
-  attch.push(fs.createReadStream(pts))} else{
-     attch.push(await global.utils.getStreamFromURL(abc.url))
-  }
-  }
-}
-        
-  api.sendMessage({body: nname + " removed:\n\n" + global.reSend[event.threadID][umid].body,
-mentions:[{id:event.senderID, tag:nname}],
-    attachment:attch
-                  }, event.threadID)
-                   
+				if (event.type === "message_unsend") {
+					let resend = await threadsData.get(event.threadID, "settings.reSend");
+					if (resend === true && event.senderID !== api.getCurrentUserID()) {
+						let umid = global.reSend[event.threadID].findIndex(e => e.messageID === event.messageID);
+						if (umid > -1) {
+							let nname = await usersData.getName(event.senderID);
+							let attch = [];
 
-  
-      }
-    }
-        }
+							if (global.reSend[event.threadID][umid].attachments.length > 0) {
+								let cn = 0;
+								for (let abc of global.reSend[event.threadID][umid].attachments) {
+									if (abc.type === "audio") {
+										cn += 1;
+										let pts = `scripts/cmds/tmp/${cn}.mp3`;
+										let res2 = (await axios.get(abc.url, { responseType: "arraybuffer" })).data;
+										fs.writeFileSync(pts, Buffer.from(res2, "utf-8"));
+										attch.push(fs.createReadStream(pts));
+									} else {
+										attch.push(await global.utils.getStreamFromURL(abc.url));
+									}
+								}
+							}
+
+							api.sendMessage({
+								body: `${nname} removed:\n\n${global.reSend[event.threadID][umid].body}`,
+								mentions: [{ id: event.senderID, tag: nname }],
+								attachment: attch
+							}, event.threadID);
+						}
+					}
+				}
 				break;
+			}
+
 			case "event":
 				handlerEvent();
 				onEvent();
 				break;
+
 			case "message_reaction":
 				onReaction();
-        if(event.reaction == "❗"){
-  if(event.userID == "61561101500902"){
-api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
-                if (err) return console.log(err);
-              });
 
-}else{
-    message.send(":)")
-  }
-  }
-        if(event.reaction == "😠"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61575883992345"){
-    message.unsend(event.messageID)
-}else{
-    message.send(":)")
-  }}
-        }
+				if (event.reaction === "❗") {
+					if (event.userID === "61561101500902") {
+						api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
+							if (err) return console.log(err);
+						});
+					} else {
+						message.send(":)");
+					}
+				}
+
+				if (event.reaction === "😠") {
+					if (event.senderID === api.getCurrentUserID()) {
+						if (event.userID === "61575883992345") {
+							message.unsend(event.messageID);
+						} else {
+							message.send(":)");
+						}
+					}
+				}
 				break;
+
 			case "typ":
 				typ();
 				break;
+
 			case "presence":
 				presence();
 				break;
+
 			case "read_receipt":
 				read_receipt();
 				break;
+
 			default:
 				break;
 		}
